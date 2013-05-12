26bd248d7ce3b98267da99a9cff9dfa9f5848b7c
Paul Bellamy <paul.a.bellamy@gmail.com>
2013-05-12 11:38:00 +0100

    redoing static file serving

--- static.go
+++ static.go
@@ -1,45 +1,37 @@
 package mango
 
 import (
-	"io/ioutil"
-	"os"
-	"path"
+	"net/http"
+	"strings"
 )
 
-func fileIsRegular(fi os.FileInfo) bool {
-	return fi.Mode()&(os.ModeDir|os.ModeSymlink|os.ModeNamedPipe|os.ModeSocket|os.ModeDevice) == 0
-}
-
-func fileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if err != nil {
-		return false
-	} else if !fileIsRegular(info) {
-		return false
+func fileExists(root http.FileSystem, path string) bool {
+	if file, err := root.Open(path); err == nil {
+		file.Close()
+		return true
 	}
 
-	return true
+	return false
 }
 
-func readFile(filename string) (string, error) {
-	body, err := ioutil.ReadFile(filename)
-	return string(body), err
-}
+func Static(directory string, f http.HandlerFunc) http.HandlerFunc {
+	root := http.Dir(directory)
+	fileServer := http.FileServer(root)
 
-func Static(directory string) Middleware {
-	return func(env Env, app App) (Status, Headers, Body) {
-		// See if we can serve a file
-		file := path.Join(directory, env.Request().URL.Path)
-		if fileExists(file) && (env.Request().Method == "GET" || env.Request().Method == "HEAD") {
-			if body, err := readFile(file); err == nil {
-				mime_type := []string{MimeType(path.Ext(file), "application/octet-stream")}
-				return 200, Headers{"Content-Type": mime_type}, Body(body)
-			} else {
-				panic(err)
-			}
+	return func(w http.ResponseWriter, r *http.Request) {
+		upath := r.URL.Path
+		if !strings.HasPrefix(upath, "/") {
+			upath = "/" + upath
 		}
 
-		// No file found, pass on to app
-		return app(env)
+		// See if we can serve a file
+		if fileExists(root, upath) && (r.Method == "GET" || r.Method == "HEAD") {
+			fileServer.ServeHTTP(w, r)
+		} else {
+			// No file found, pass on to app
+			if f != nil {
+				f(w, r)
+			}
+		}
 	}
 }
--- static_test.go
+++ static_test.go
@@ -7,19 +7,22 @@ import (
 	"testing"
 )
 
-func staticTestServer(env Env) (Status, Headers, Body) {
-	return 200, Headers{"Content-Type": []string{"text/html"}}, Body("<h1>Hello World!</h1>")
+func staticTestServer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1>Hello World!</h1>"))
 }
 
 func TestStaticSuccess(t *testing.T) {
 	// Compile the stack
-	staticStack := new(Stack)
-	staticStack.Middleware(Static("./static"))
-	staticApp := staticStack.Compile(staticTestServer)
+	app := Static("./static", staticTestServer)
 
 	// Request against it
 	request, err := http.NewRequest("GET", "http://localhost:3000/static.html", nil)
-	status, headers, body := staticApp(Env{"mango.request": &Request{request}})
+	response := NewMockResponseWriter()
+	app(response, request)
+	status := response.Status
+	headers := response.Header()
+	body := response.Body.String()
 
 	if err != nil {
 		t.Error(err)
@@ -30,26 +33,52 @@ func TestStaticSuccess(t *testing.T) {
 	}
 
 	expected := "<h1>I'm a static test file</h1>\n"
-	if string(body) != expected {
-		t.Error("Expected body:", string(body), "to equal:", expected)
+	if body != expected {
+		t.Error("Expected body:", body, "to equal:", expected)
 	}
 
-	expected = "text/html"
+	expected = "text/html; charset=utf-8"
 	got := headers.Get("Content-Type")
 	if got != expected {
 		t.Error("Expected Content-Type:", got, "to equal:", expected)
 	}
 }
 
-func TestStaticFail(t *testing.T) {
+func TestStaticNoUpstream(t *testing.T) {
 	// Compile the stack
-	staticStack := new(Stack)
-	staticStack.Middleware(Static("./static"))
-	staticApp := staticStack.Compile(staticTestServer)
+	app := Static("./static", nil)
 
 	// Request against it
 	request, err := http.NewRequest("GET", "http://localhost:3000/not_a_file.html", nil)
-	status, _, body := staticApp(Env{"mango.request": &Request{request}})
+	response := NewMockResponseWriter()
+	app(response, request)
+	status := response.Status
+	body := response.Body.String()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if status != 0 {
+		t.Error("Expected status to equal 0, got:", status)
+	}
+
+	expected := ""
+	if body != expected {
+		t.Error("Expected body:", body, "to equal:", expected)
+	}
+}
+
+func TestStaticFail(t *testing.T) {
+	// Compile the stack
+	app := Static("./static", staticTestServer)
+
+	// Request against it
+	request, err := http.NewRequest("GET", "http://localhost:3000/not_a_file.html", nil)
+	response := NewMockResponseWriter()
+	app(response, request)
+	status := response.Status
+	body := response.Body.String()
 
 	if err != nil {
 		t.Error(err)
@@ -60,20 +89,21 @@ func TestStaticFail(t *testing.T) {
 	}
 
 	expected := "<h1>Hello World!</h1>"
-	if string(body) != expected {
-		t.Error("Expected body:", string(body), "to equal:", expected)
+	if body != expected {
+		t.Error("Expected body:", body, "to equal:", expected)
 	}
 }
 
 func TestStaticBinaryFile(t *testing.T) {
 	// Compile the stack
-	staticStack := new(Stack)
-	staticStack.Middleware(Static("./static"))
-	staticApp := staticStack.Compile(staticTestServer)
+	app := Static("./static", staticTestServer)
 
 	// Request against it
 	request, err := http.NewRequest("GET", "http://localhost:3000/binary_file.png", nil)
-	status, _, body := staticApp(Env{"mango.request": &Request{request}})
+	response := NewMockResponseWriter()
+	app(response, request)
+	status := response.Status
+	body := response.Body.String()
 
 	if err != nil {
 		t.Error(err)
@@ -96,15 +126,14 @@ func TestStaticBinaryFile(t *testing.T) {
 func BenchmarkStatic(b *testing.B) {
 	b.StopTimer()
 
-	staticStack := new(Stack)
-	staticStack.Middleware(Static("./static"))
-	staticApp := staticStack.Compile(staticTestServer)
+	app := Static("./static", staticTestServer)
 
 	request, _ := http.NewRequest("GET", "http://localhost:3000/static.html", nil)
 
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		staticApp(Env{"mango.request": &Request{request}})
+		response := NewMockResponseWriter()
+		app(response, request)
 	}
 	b.StopTimer()
 }
