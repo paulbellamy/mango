6b7f66d00b1eef254b9244a37a8efb73d4f29ce5
Paul Bellamy <paul.a.bellamy@gmail.com>
2013-05-12 14:43:09 +0100

    adding an example with multiple middlewares

---
+++ examples/middleware/middleware.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	. "../../" // Point this to mango
+	"log"
+	"net/http"
+	"time"
+)
+
+// Let's define a custom middleware to do some logging
+func Logger(app http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		app(w, r)
+		log.Println(r.Method, r.URL.Path, time.Since(start))
+	}
+}
+
+// An upstream app to call
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func main() {
+
+	// Initialize our stack of middleware
+	app :=
+		Logger(
+			ShowErrors("",
+				Static("./static",
+					Hello)))
+
+	http.HandleFunc("/", app)
+	http.ListenAndServe(":3000", nil)
+}
